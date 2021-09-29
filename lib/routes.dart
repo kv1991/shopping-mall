8d0c120de5da5c52cd576304c3dcff81ed40f8f8
Kaiwen Tian <kaiwen.tian@wishlife.com>
2021-09-29 16:12:26 +0800

    feat:  added login route.

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_mall/screens/sign_in/sign_in_screen.dart';
 
 import 'package:shopping_mall/screens/splash/splash_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
   SplashScreen.routeName: (context) => SplashScreen(),
+  SignInScreen.routeName: (context) => SignInScreen(),
 };
