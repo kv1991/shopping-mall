39f8aca164d2b2a22db4bd2b81710873fddfab73
Kaiwen Tian <kaiwen.tian@wishlife.com>
2021-09-29 16:09:01 +0800

    feat:  added indicators UI for list view.

--- lib/screens/splash/components/body.dart
+++ lib/screens/splash/components/body.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_mall/components/default_button.dart';
+import 'package:shopping_mall/constants.dart';
 import 'package:shopping_mall/screens/sign_in/sign_in_screen.dart';
 import 'package:shopping_mall/screens/splash/components/splash_content.dart';
 import 'package:shopping_mall/size_config.dart';
-
 
 class Body extends StatefulWidget {
   @override
@@ -12,7 +12,7 @@ class Body extends StatefulWidget {
 
 class _BodyState extends State<Body> {
   int currentPage = 0;
-  List<Map<String, String>>splashData = [
+  List<Map<String, String>> splashData = [
     {
       "text": "Welcome to Tokoto, Let’s shop!",
       "image": "assets/images/splash_1.png"
@@ -32,44 +32,52 @@ class _BodyState extends State<Body> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: SizedBox(
-        // width: double.infinity,
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              flex: 3,
-              child: PageView.builder(
-                onPageChanged: (value) {
-                  setState(() => {
-                    currentPage = value
-                  });
-                },
-                itemCount: splashData.length,
-                itemBuilder: (context, index) => SplashContent(image: splashData[index]['image'], text: splashData[index]['text']),
-              )
-            ),
-            Expanded(
-              flex: 2,
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
-                child: Column(
-                  children: <Widget>[
-                    Spacer(),
-                    Spacer(),
-                    DefaultButton(
-                      text: 'Continue',
-                      press: () => {
-                        Navigator.pushNamed(context, SignInScreen.routeName)
-                      }
-                    )
-                    // Row()
-                  ]
-                ),
-              ),
-            ),
-          ]
-        )
-      ),
+          // width: double.infinity,
+          child: Column(children: <Widget>[
+        Expanded(
+            flex: 3,
+            child: PageView.builder(
+              onPageChanged: (value) {
+                setState(() => {currentPage = value});
+              },
+              itemCount: splashData.length,
+              itemBuilder: (context, index) => SplashContent(
+                  image: splashData[index]['image'],
+                  text: splashData[index]['text']),
+            )),
+        Expanded(
+          flex: 2,
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: getProportionateScreenWidth(20)),
+            child: Column(children: <Widget>[
+              Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                  children: List.generate(
+                      splashData.length, (index) => buildDot(index: index))),
+                      Spacer(flex: 3),
+              DefaultButton(
+                  text: 'Continue',
+                  press: () =>
+                      {Navigator.pushNamed(context, SignInScreen.routeName)}),
+              Spacer()
+            ]),
+          ),
+        ),
+      ])),
     );
   }
-}
 
+  AnimatedContainer buildDot({int index}) {
+    return AnimatedContainer(
+        duration: kAnimationDuration,
+        margin: EdgeInsets.only(right: 5),
+        width: currentPage == index ? 20 : 6,
+        height: 6,
+        decoration: BoxDecoration(
+          color: currentPage == index ? kPrimaryColor : Color(0xFFD8D8D8),
+          borderRadius: BorderRadius.circular(3),
+        ));
+  }
+}
