fe96e0337a314918d3f6e79ab1c37ca14b0d7f27
Kaiwen Tian <kaiwen.tian@wishlife.com>
2021-09-30 14:17:05 +0800

    style:  updated global theme UI.

--- lib/theme.dart
+++ lib/theme.dart
@@ -15,7 +15,7 @@ ThemeData theme() {
 
 InputDecorationTheme inputDecorationTheme() {
   OutlineInputBorder outlineInputBorder = OutlineInputBorder(
-    borderRadius: BorderRadius.circular(28),
+    borderRadius: BorderRadius.circular(50),
     borderSide: BorderSide(color: kTextColor),
     gapPadding: 10,
   );
@@ -25,6 +25,7 @@ InputDecorationTheme inputDecorationTheme() {
     enabledBorder: outlineInputBorder,
     focusedBorder: outlineInputBorder,
     border: outlineInputBorder,
+    labelStyle: TextStyle(fontSize: 18)
   );
 }
 
@@ -38,6 +39,11 @@ TextTheme textTheme() {
 AppBarTheme appBarTheme() {
   return AppBarTheme(
     color: Colors.white,
-    elevation: 0
+    elevation: 0,
+    // brightness: Brightness.light,
+    iconTheme: IconThemeData(color: Colors.black),
+    // textTheme: TextTheme(
+    //   headline6: TextStyle(color: Color(0XFF8B8B8B), fontSize: 18),
+    // ),
   );
 }
