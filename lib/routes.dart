8c577860cb893d3e2d39d1086e645150664cb059
Kaiwen Tian <kaiwen.tian@wishlife.com>
2021-10-09 16:54:19 +0800

    feat:  added sign up route page.

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shopping_mall/screens/forgot_password/forgot_password_screen.dart';
 import 'package:shopping_mall/screens/login_success/login_success_screen.dart';
 import 'package:shopping_mall/screens/sign_in/sign_in_screen.dart';
+import 'package:shopping_mall/screens/sign_up/sign_up_screen.dart';
 
 import 'package:shopping_mall/screens/splash/splash_screen.dart';
 
@@ -10,4 +11,5 @@ final Map<String, WidgetBuilder> routes = {
   SignInScreen.routeName: (context) => SignInScreen(),
   LoginSuccessScreen.routeName: (context) => LoginSuccessScreen(),
   ForgotPasswordScreen.routeName: (context) => ForgotPasswordScreen(),
+  SignUpScreen.routeName: (context) => SignUpScreen(),
 };
