dc8f692d05329746f9a14c667561d2a5eff00a0c
Kaiwen Tian <kaiwen.tian@wishlife.com>
2021-10-12 14:44:03 +0800

    style:  updated UI of app bar title.

--- lib/theme.dart
+++ lib/theme.dart
@@ -32,7 +32,7 @@ InputDecorationTheme inputDecorationTheme() {
 TextTheme textTheme() {
   return TextTheme(
     bodyText1: TextStyle(color: kTextColor),
-    bodyText2: TextStyle(color: kTextColor)
+    bodyText2: TextStyle(color: kSecondaryColor)
   );
 }
 
@@ -40,10 +40,7 @@ AppBarTheme appBarTheme() {
   return AppBarTheme(
     color: Colors.white,
     elevation: 0,
-    // brightness: Brightness.light,
-    iconTheme: IconThemeData(color: Colors.black),
-    // textTheme: TextTheme(
-    //   headline6: TextStyle(color: Color(0XFF8B8B8B), fontSize: 18),
-    // ),
+    titleTextStyle: TextStyle(color: Colors.black54, fontSize: 18),
+    iconTheme: IconThemeData(color: Colors.black)
   );
 }
