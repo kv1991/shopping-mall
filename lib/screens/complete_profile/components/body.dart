52fe548076c6fdae96c48f141b3442a8c6b57f3b
Kaiwen Tian <kaiwen.tian@wishlife.com>
2021-10-12 22:20:00 +0800

    feat: added basic app bar title for complete profile route page.

--- lib/screens/complete_profile/components/body.dart
+++ lib/screens/complete_profile/components/body.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_mall/constants.dart';
+import 'package:shopping_mall/size_config.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            children: [
+                SizedBox(height: SizeConfig.screenHeight * 0.03),
+                Text("Complete Profile", style: headingStyle),
+                Text(
+                  "Complete your details or continue  \nwith social media",
+                  textAlign: TextAlign.center,
+                ),
+            ],
+          )
+        )
+      ),
+    );
   }
 }
