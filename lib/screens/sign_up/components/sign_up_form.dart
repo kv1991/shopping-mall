30c9962aa4fd8216e08c2b874eb72bc16877114c
Kaiwen Tian <kaiwen.tian@wishlife.com>
2021-10-12 21:47:01 +0800

    feat:  completed sign up form.

--- lib/screens/sign_up/components/sign_up_form.dart
+++ lib/screens/sign_up/components/sign_up_form.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shopping_mall/components/custom_surfix_icon.dart';
 import 'package:shopping_mall/components/default_button.dart';
 import 'package:shopping_mall/components/form_errors.dart';
 import 'package:shopping_mall/constants.dart';
-import 'package:shopping_mall/screens/sign_in/sign_in_screen.dart';
+import 'package:shopping_mall/helper/keyboard.dart';
+import 'package:shopping_mall/screens/login_success/login_success_screen.dart';
 import 'package:shopping_mall/size_config.dart';
 
 class SignUpForm extends StatefulWidget {
@@ -12,123 +14,165 @@ class SignUpForm extends StatefulWidget {
 }
 
 class _SignUpFormState extends State<SignUpForm> {
-  final _formKey = GlobalKey<FormState>();
+  bool remember = false;
+  bool isObscureText = true;
   String email;
   String password;
-  String conformPassword;
-  List<String> formErrors = [];
+  String confirmPassword;
+  String iconPath = 'Lock';
+  final _formKey = GlobalKey<FormState>();
+  final List <String>errors = [];
+  TextEditingController passwordEditingController = TextEditingController();
 
   void addError({ String error }) {
-    if(!formErrors.contains(error)) {
+    if(!errors.contains(error)) {
       setState(() {
-        formErrors.add(error);
+        errors.add(error);
       });
     }
   }
 
   void removeError({ String error }) {
-    if(formErrors.contains(error)) {
+    print(error);
+    print(errors.contains(error));
+    if(errors.contains(error)) {
       setState(() {
-        formErrors.remove(error);
+        print(error);
+        errors.remove(error);
       });
     }
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          SizedBox(height: getProportionateScreenHeight(40)),
-          renderEmailTextField(),
+          buildEmailFormField(),
           SizedBox(height: getProportionateScreenHeight(30)),
-          renderPasswordTextField(),
+          buildPasswordFormField(),
           SizedBox(height: getProportionateScreenHeight(30)),
-          renderConfPasswordTextField(),
+          buildConfPasswordFormField(),
           SizedBox(height: getProportionateScreenHeight(30)),
-          FormErrors(errors: formErrors),
+          FormErrors(errors: errors),
+          SizedBox(height: getProportionateScreenHeight(20)),
           DefaultButton(
             text: 'Continue',
             press: () {
               if (_formKey.currentState.validate()) {
                 _formKey.currentState.save();
-                Navigator.pushNamed(context, SignInScreen.routeName);
+                KeyboardUtil.hideKeyboard(context);
+                Navigator.pushNamed(context, LoginSuccessScreen.routeName);
               }
             }
           )
-        ],
+        ]
       )
     );
   }
 
-  Widget renderEmailTextField() {
+  TextFormField buildEmailFormField() {
     return TextFormField(
       keyboardType: TextInputType.emailAddress,
-      decoration: InputDecoration(
-        labelText: 'Email',
-        hintText: 'Enter your email address',
-        suffixIcon: CustomSurffixIcon(svgIcon: 'assets/icons/Mail.svg')
-      ),
-      onSaved: (val) => val = email,
-      onChanged: (val) {
-        if(val.isNotEmpty) {
+      onSaved: (newValue) => newValue = email,
+      onChanged: (value) {
+        if(value.isNotEmpty) {
           removeError(error: kEmailNullError);
-        } else if(emailValidatorRegExp.hasMatch(val)) {
+        } else if(emailValidatorRegExp.hasMatch(value)) {
           removeError(error: kInvalidEmailError);
         }
-        setState(() {
-          val = email;
-        });
       },
-      validator: (val) {
-        if(val.isEmpty) {
+      validator: (value) {
+        if(value.isEmpty) {
           addError(error: kEmailNullError);
           return '';
-        } else if(emailValidatorRegExp.hasMatch(val)) {
+        } else if(!emailValidatorRegExp.hasMatch(value)) {
           addError(error: kInvalidEmailError);
           return '';
         }
         return null;
       },
+      decoration: InputDecoration(
+        labelText: 'Eamil',
+        hintText: 'Enter your email',
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: CustomSurffixIcon(svgIcon: 'assets/icons/Mail.svg')
+      )
     );
   }
 
-  Widget renderPasswordTextField() {
+  TextFormField buildPasswordFormField() {
     return TextFormField(
-      obscureText: true,
-      decoration: InputDecoration(
-        labelText: 'Password',
-        hintText: 'Enter your password',
-        suffixIcon: CustomSurffixIcon(svgIcon: 'assets/icons/Lock.svg')
-      ),
-      onSaved: (val) => val = password,
-      onChanged: (val) {
-        if(val.isNotEmpty) {
+      obscureText: isObscureText,
+      onSaved: (newValue) => newValue = password,
+      controller: passwordEditingController,
+      onChanged: (value) {
+        if(value.isEmpty) {
+          setState(() {
+            iconPath = 'Lock';
+          });
+        }
+        if(value.isNotEmpty) {
           removeError(error: kPassNullError);
-          return '';
-        } else if(val.length >= 8) {
+          setState(() {
+            iconPath = 'eye-closed';
+          });
+        } else if(value.length >= 8) {
           removeError(error: kShortPassError);
-          return '';
         }
         setState(() {
-          val = password;
+          password = value;
         });
       },
-      validator: (val) {
-        if(val.isEmpty) {
+      validator: (value) {
+        if(value.isEmpty) {
           addError(error: kPassNullError);
           return '';
-        } else if(val.length < 8) {
+        } else if(value.length < 8) {
+          addError(error: kShortPassError);
+          return '';
+        } else if(value.length < 8) {
           addError(error: kShortPassError);
           return '';
         }
         return null;
       },
+      decoration: InputDecoration(
+        labelText: 'Password',
+        hintText: 'Enter your password',
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: Padding(
+          padding: EdgeInsets.only(right: getProportionateScreenWidth(10)),
+          child: IconButton(
+          // icon: CustomSurffixIcon(svgIcon: 'assets/icons/Lock.svg'),
+          icon: SvgPicture.asset(
+            'assets/icons/${iconPath}.svg',
+            width: getProportionateScreenWidth(20)
+          ),
+          onPressed: () {
+            setState(() {
+              if(passwordEditingController.text.isNotEmpty) {
+                if(isObscureText) {
+                  iconPath = 'eye-open';
+                } else {
+                  iconPath = 'eye-closed';
+                }
+                isObscureText = !isObscureText;
+              } else {
+                iconPath = 'Lock';
+                isObscureText = true;
+              }
+            });
+          },
+        )
+        )
+      )
     );
   }
 
-  Widget renderConfPasswordTextField() {
+  Widget buildConfPasswordFormField() {
     return TextFormField(
       obscureText: true,
       decoration: InputDecoration(
@@ -136,27 +180,17 @@ class _SignUpFormState extends State<SignUpForm> {
         hintText: 'Enter your confirm password',
         suffixIcon: CustomSurffixIcon(svgIcon: 'assets/icons/Lock.svg')
       ),
-      onSaved: (val) => val = conformPassword,
+      onSaved: (val) => val = confirmPassword,
       onChanged: (val) {
         if(val.isNotEmpty) {
           removeError(error: kPassNullError);
-          return '';
-        } else if(val.length >= 8) {
-          removeError(error: kShortPassError);
-          return '';
-        } else if (val == password) {
+        } else if (val.isNotEmpty && val == password) {
           removeError(error: kMatchPassError);
         }
-        setState(() {
-          val = password;
-        });
       },
       validator: (val) {
         if(val.isEmpty) {
           addError(error: kPassNullError);
-          return '';
-        } else if(val.length < 8) {
-          addError(error: kShortPassError);
           return '';
         } else if (val != password) {
           addError(error: kMatchPassError);
