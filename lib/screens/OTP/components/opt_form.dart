acd643862db3ffef348c5e10588db0fe6ef9e97e
Kaiwen Tian <kaiwen.tian@wishlife.com>
2021-10-13 17:47:46 +0800

    style:  updated OTP input field UI and added obscure type.

--- lib/screens/OTP/components/opt_form.dart
+++ lib/screens/OTP/components/opt_form.dart
@@ -5,43 +5,53 @@ import 'package:shopping_mall/size_config.dart';
 class OPTForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Form(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            SizedBox(height: SizeConfig.screenHeight * 0.2),
-            SizedBox(
-              width: getProportionateScreenWidth(60),
-              child: TextFormField(
-                autofocus: true,
-                textAlign: TextAlign.center,
-                decoration: otpInputDecoration,
+    return Form(
+      child: Column(
+        children: [
+          SizedBox(height: SizeConfig.screenHeight * 0.1),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(
+                width: getProportionateScreenWidth(60),
+                child: TextFormField(
+                  autofocus: true,
+                  obscureText: true,
+                  style: TextStyle(fontSize: 24),
+                  textAlign: TextAlign.center,
+                  decoration: otpInputDecoration,
+                ),
               ),
-            ),
-            SizedBox(
-              width: getProportionateScreenWidth(60),
-              child: TextFormField(
-                textAlign: TextAlign.center,
-                decoration: otpInputDecoration,
+              SizedBox(
+                width: getProportionateScreenWidth(60),
+                child: TextFormField(
+                  obscureText: true,
+                  style: TextStyle(fontSize: 24),
+                  textAlign: TextAlign.center,
+                  decoration: otpInputDecoration,
+                ),
               ),
-            ),
-            SizedBox(
-              width: getProportionateScreenWidth(60),
-              child: TextFormField(
-                textAlign: TextAlign.center,
-                decoration: otpInputDecoration,
+              SizedBox(
+                width: getProportionateScreenWidth(60),
+                child: TextFormField(
+                  obscureText: true,
+                  style: TextStyle(fontSize: 24),
+                  textAlign: TextAlign.center,
+                  decoration: otpInputDecoration,
+                ),
               ),
-            ),
-            SizedBox(
-              width: getProportionateScreenWidth(60),
-              child: TextFormField(
-                textAlign: TextAlign.center,
-                decoration: otpInputDecoration,
+              SizedBox(
+                width: getProportionateScreenWidth(60),
+                child: TextFormField(
+                  obscureText: true,
+                  style: TextStyle(fontSize: 24),
+                  textAlign: TextAlign.center,
+                  decoration: otpInputDecoration,
+                ),
               ),
-            ),
-          ]
-        ),
+            ]
+          ),
+        ],
       ),
     );
   }
