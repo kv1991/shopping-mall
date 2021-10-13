d052037cfb4c525da8a8d26c656a127b2dc06c48
Kaiwen Tian <kaiwen.tian@wishlife.com>
2021-10-13 18:09:49 +0800

    feat:  added field input logic.

--- lib/screens/OTP/components/opt_form.dart
+++ lib/screens/OTP/components/opt_form.dart
@@ -2,7 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:shopping_mall/constants.dart';
 import 'package:shopping_mall/size_config.dart';
 
-class OPTForm extends StatelessWidget {
+class OPTForm extends StatefulWidget {
+  @override
+_OPTFormState createState() => _OPTFormState();
+}
+
+class _OPTFormState extends State<OPTForm> {
+  FocusNode pin2FocusNode;
+  FocusNode pin3FocusNode;
+  FocusNode pin4FocusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    pin2FocusNode = FocusNode();
+    pin3FocusNode = FocusNode();
+    pin4FocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pin2FocusNode.dispose();
+    pin3FocusNode.dispose();
+    pin4FocusNode.dispose();
+  }
+
+  void nextField(String value, FocusNode focusNode) {
+    if(value.length == 1) {
+      focusNode.requestFocus();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -17,36 +48,55 @@ class OPTForm extends StatelessWidget {
                 child: TextFormField(
                   autofocus: true,
                   obscureText: true,
+                  keyboardType: TextInputType.number,
                   style: TextStyle(fontSize: 24),
                   textAlign: TextAlign.center,
                   decoration: otpInputDecoration,
+                  onChanged: (value) {
+                    nextField(value, pin2FocusNode);
+                  }
                 ),
               ),
               SizedBox(
                 width: getProportionateScreenWidth(60),
                 child: TextFormField(
                   obscureText: true,
+                  keyboardType: TextInputType.number,
+                  focusNode: pin2FocusNode,
                   style: TextStyle(fontSize: 24),
                   textAlign: TextAlign.center,
                   decoration: otpInputDecoration,
+                  onChanged: (value) {
+                    nextField(value, pin3FocusNode);
+                  }
                 ),
               ),
               SizedBox(
                 width: getProportionateScreenWidth(60),
                 child: TextFormField(
                   obscureText: true,
+                  keyboardType: TextInputType.number,
+                  focusNode: pin3FocusNode,
                   style: TextStyle(fontSize: 24),
                   textAlign: TextAlign.center,
                   decoration: otpInputDecoration,
+                  onChanged: (value) {
+                    nextField(value, pin4FocusNode);
+                  }
                 ),
               ),
               SizedBox(
                 width: getProportionateScreenWidth(60),
                 child: TextFormField(
                   obscureText: true,
+                  keyboardType: TextInputType.number,
+                  focusNode: pin4FocusNode,
                   style: TextStyle(fontSize: 24),
                   textAlign: TextAlign.center,
                   decoration: otpInputDecoration,
+                  onChanged: (value) {
+                    pin4FocusNode.unfocus();
+                  }
                 ),
               ),
             ]
