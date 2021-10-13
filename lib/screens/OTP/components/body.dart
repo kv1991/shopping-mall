0e7a0cdc209155edad72125839793524c8cec2ef
Kaiwen Tian <kaiwen.tian@wishlife.com>
2021-10-13 16:26:49 +0800

    feat:  added tween animation component widget.

--- lib/screens/OTP/components/body.dart
+++ lib/screens/OTP/components/body.dart
@@ -1,8 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_mall/constants.dart';
+import 'package:shopping_mall/size_config.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+          child: Column(
+            children: [
+              Text(
+                "OTP Verification",
+                style: headingStyle,
+              ),
+              Text("We sent your code to +1 898 860 ***"),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("This code will expired in "),
+                  TweenAnimationBuilder(
+                    tween: Tween(begin: 30.0, end: 0.0),
+                    duration: Duration(seconds: 30),
+                    builder: (_, int, child) => Text("00:${int.toInt()}", style: TextStyle(color: kPrimaryColor)),
+                  )
+                ]
+              )
+            ]
+          )
+        )
+      )
+    );
   }
 }
