0ae4532869e7141415298f592c4ec19278177cc8
Kaiwen Tian <kaiwen.tian@wishlife.com>
2021-10-18 17:31:10 +0800

    feat:  added categories UI.

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -97,11 +97,74 @@ class Body extends StatelessWidget {
                 ]
               ),
               SizedBox(height: getProportionateScreenHeight(20)),
-              DiscountBanner()
+              DiscountBanner(),
+              SizedBox(height: getProportionateScreenHeight(20)),
+              Categories()
             ]
           )
         )
       )
+    );
+  }
+}
+
+class Categories extends StatelessWidget {
+  final List <Map<String, String>>categories = [
+    {"icon": "assets/icons/Flash Icon.svg", "text": "Flash Deal"},
+    {"icon": "assets/icons/Bill Icon.svg", "text": "Bill"},
+    {"icon": "assets/icons/Game Icon.svg", "text": "Game"},
+    {"icon": "assets/icons/Gift Icon.svg", "text": "Daily Gift"},
+    {"icon": "assets/icons/Discover.svg", "text": "More"}
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        ...List.generate(categories.length, (index) => CategoryCart(icon: categories[index]['icon'], text: categories[index]['text']))
+      ]
+    );
+  }
+}
+
+class CategoryCart extends StatelessWidget {
+  final String icon;
+  final String text;
+  const CategoryCart({
+    Key key,
+    @required this.icon,
+    @required this.text
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: SizedBox(
+        width: getProportionateScreenWidth(56),
+        child: Column(
+          children: [
+            AspectRatio(
+              aspectRatio: 1,
+              child: Container(
+                padding: EdgeInsets.all(getProportionateScreenWidth(15)),
+                decoration: BoxDecoration(
+                  color: Color(0xFFFFECDF),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                width: getProportionateScreenWidth(56),
+                height: getProportionateScreenHeight(56),
+                child: SvgPicture.asset(icon)
+              ),
+            ),
+            SizedBox(
+              height: getProportionateScreenHeight(5),
+              child: Text(text)
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
