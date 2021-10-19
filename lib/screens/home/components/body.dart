3ff97c6044d26a397c15e24a03ce912814584457
Kaiwen Tian <kaiwen.tian@wishlife.com>
2021-10-19 22:17:59 +0800

    chore: extract categories widget file.

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shopping_mall/constants.dart';
+import 'package:shopping_mall/screens/home/components/categories.dart';
 import 'package:shopping_mall/screens/home/components/discount_banner.dart';
 import 'package:shopping_mall/screens/home/components/special_offers.dart';
 import 'package:shopping_mall/size_config.dart';
@@ -105,68 +106,6 @@ class Body extends StatelessWidget {
               SpecialOffers(),
             ]
           )
-        )
-      )
-    );
-  }
-}
-
-class Categories extends StatelessWidget {
-  final List <Map<String, String>>categories = [
-    {"icon": "assets/icons/Flash Icon.svg", "text": "Flash Deal"},
-    {"icon": "assets/icons/Bill Icon.svg", "text": "Bill"},
-    {"icon": "assets/icons/Game Icon.svg", "text": "Game"},
-    {"icon": "assets/icons/Gift Icon.svg", "text": "Daily Gift"},
-    {"icon": "assets/icons/Discover.svg", "text": "More"}
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        ...List.generate(categories.length, (index) => CategoryCart(icon: categories[index]['icon'], text: categories[index]['text']))
-      ]
-    );
-  }
-}
-
-class CategoryCart extends StatelessWidget {
-  final String icon;
-  final String text;
-  const CategoryCart({
-    Key key,
-    @required this.icon,
-    @required this.text
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {},
-      child: SizedBox(
-        width: getProportionateScreenWidth(55),
-        child: Column(
-          children: [
-            AspectRatio(
-              aspectRatio: 1,
-              child: Container(
-                padding: EdgeInsets.all(getProportionateScreenWidth(15)),
-                decoration: BoxDecoration(
-                  color: Color(0xFFFFECDF),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: SvgPicture.asset(icon)
-              ),
-            ),
-            const SizedBox(height: 5),
-            Text(
-              text,
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.black)
-            )
-          ]
         )
       )
     );
