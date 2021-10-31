4bafdc1d0d48d267532931996949ed6b2b4b4127
Kaiwen Tian <kaiwen.tian@wishlife.com>
2021-10-31 13:36:27 +0800

    feat:  built adding to cart button widget.

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_mall/components/default_button.dart';
 import 'package:shopping_mall/models/Product.dart';
 import 'package:shopping_mall/screens/details/components/color_dots.dart';
 import 'package:shopping_mall/screens/details/components/product_descriptions.dart';
 import 'package:shopping_mall/screens/details/components/product_images.dart';
 import 'package:shopping_mall/screens/details/components/top_rounded_container.dart';
+import 'package:shopping_mall/size_config.dart';
 
 class Body extends StatelessWidget {
   final Product product;
@@ -11,7 +13,7 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ListView(
       children: [
         ProductImages(product: product),
         TopRoundedContainer(
@@ -23,7 +25,22 @@ class Body extends StatelessWidget {
                 color: Color(0xFFF6F7F9),
                 child: Column(
                   children: [
-                    ColorDots(product: product)
+                    ColorDots(product: product),
+                    TopRoundedContainer(
+                      color: Colors.white,
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                          left: SizeConfig.screenWidth * 0.05,
+                          right: SizeConfig.screenWidth * 0.05,
+                          bottom: getProportionateScreenWidth(40),
+                          top: getProportionateScreenWidth(15)
+                        ),
+                        child: DefaultButton(
+                          text: 'Add to Cart',
+                          press: () {}
+                        ),
+                      )
+                    )
                   ]
                 )
               )
